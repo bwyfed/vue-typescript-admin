fd53f2e0d651bb8a38de72c41938c9f2fa0cf48b
bwyfed <36172659@qq.com>
2020-02-07 16:45:16 +0800

    实现按需引入elementt-ui组件的功能和示例。

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// 按需引入 element-ui 的组件
+import { Button, Row } from "element-ui";
+
+Vue.component(Button.name, Button);
+Vue.component(Row.name, Row);
+
 new Vue({
   router,
   store,
